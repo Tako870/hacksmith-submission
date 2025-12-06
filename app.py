183e29f5362bad803eb18322a6d8212d20faef53
Tako870 <141704109+Tako870@users.noreply.github.com>
2025-12-06 22:04:42 +0800

    Created base paths

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Future Black Hat Arsenal Exhibitors.'
+
+@app.route('/uploadmap')
+def upload_asset_map():
+    return render_template('uploadMap.html')
+
+@app.route('/rendermap')
+def render_map():
+    return "placeholder"
 
 if __name__ == '__main__':
     app.run(debug=True)
